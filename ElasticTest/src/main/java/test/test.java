694151275108f37cf7da243a64a0a589d2205e37
David Pastor <66854246+Aldokler@users.noreply.github.com>
2022-10-07 00:03:42 -0600

    Eureka! solo que está capeado a los primeros 10 resultados...

--- ElasticTest/src/main/java/test/test.java
+++ ElasticTest/src/main/java/test/test.java
@@ -9,10 +9,13 @@ import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.elasticsearch._types.ElasticsearchException;
 import co.elastic.clients.elasticsearch.core.SearchResponse;
 import co.elastic.clients.elasticsearch.core.search.Hit;
+import co.elastic.clients.elasticsearch.core.search.TotalHits;
+import co.elastic.clients.elasticsearch.core.search.TotalHitsRelation;
 import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import co.elastic.clients.transport.ElasticsearchTransport;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
@@ -32,13 +35,15 @@ public class test {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException, InterruptedException{
+        System.out.println("Test");
         List<Person> maes = findByName("Ingrid");
+        System.out.println("There are " + maes.size() + " results");
         maes.forEach(mae -> {
             System.out.println(mae);
         });
+        System.out.println("Test");
     }
-    
     private static CredentialsProvider credentialsProvider(){
         final CredentialsProvider credentialsProvider =
             new BasicCredentialsProvider();
@@ -48,7 +53,7 @@ public class test {
     };
     
     private static final RestClientBuilder builder = RestClient.builder(
-        new HttpHost("investigacio-n-bdii.kb.us-central1.gcp.cloud.es.io", 9243, "https"))
+        new HttpHost("investigacio-n-bdii.es.us-central1.gcp.cloud.es.io", 9243, "https"))
         .setHttpClientConfigCallback((HttpAsyncClientBuilder httpClientBuilder) -> httpClientBuilder
                 .setDefaultCredentialsProvider(credentialsProvider()));
     
@@ -62,52 +67,54 @@ public class test {
     // And create the API client
     private static final ElasticsearchClient client = new ElasticsearchClient(transport);
     
-    public static List<Person> findByName (String name){
-        try{
-                SearchResponse<Person> search = client.search(b -> b
-                .index("person")
-                .query(q -> q
-                    .term(t -> t
-                        .field("name")
-                        .value(name)
-                    )
-                ),
-                Person.class
-            );
-                
-            List<Person> results = null;
-
-            List<Hit<Person>> hits = search.hits().hits();
-            hits.stream().map(hit -> hit.source()).forEachOrdered(results::add);
+    public static List<Person> findByName (String name) throws IOException{
+            List<Person> results = new ArrayList<>();
+            
+            try{
+                SearchResponse<Person> response = client.search(s -> s
+                    .index("person") 
+                    .query(q -> q      
+                        .match(t -> t   
+                            .field("name")  
+                            .query(name)
+                        )
+                    ),
+                    Person.class      
+                );
+                System.out.println(response.hits().total());
+                List<Hit<Person>> hits = response.hits().hits();
+                System.out.println(hits.size());
+                for (Hit<Person> hit: hits) {
+                    results.add(hit.source());
+                }
+            }catch(Exception e){
+                System.out.println(e.getMessage());
+            }
+            
             return results;  
-        }catch (ElasticsearchException | IOException e){
-            System.out.println(e.getMessage());
-        }
-        return null;
     }
     
     public static List<Person> findById (String id){
+        List<Person> results = new ArrayList<>();
         try{
                 SearchResponse<Person> search = client.search(b -> b
-                .index("person")
-                .query(q -> q
-                    .term(t -> t
-                        .field("id")
-                        .value(id)
-                    )
-                ),
-                Person.class
-            );
+                    .index("person")
+                    .query(q -> q
+                        .term(t -> t
+                            .field("id")
+                            .value(id)
+                        )
+                    ),
+                    Person.class
+                );
                 
-            List<Person> results = null;
-
             List<Hit<Person>> hits = search.hits().hits();
             hits.stream().map(hit -> hit.source()).forEachOrdered(results::add);
-            return results;  
+            
         }catch (ElasticsearchException | IOException e){
             System.out.println(e.getMessage());
         }
-        return null;
+        return results;
     }
    
 }
